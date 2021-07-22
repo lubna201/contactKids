3c9eb5033e55a3956c0819122d1d247b5ed98e7b
lubna201 <lubnasazida@gmail.com>
2021-07-22 12:09:37 +0600

    cd

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -14,44 +14,42 @@ const Home = () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
     return (
-        <StylesProvider>
-                    <div>
-                        <Container>
-                            <Typography style={{ textAlign: "center", fontWeight:"700"}} variant="h5" gutterBottom>
-                                Contact Us
-                            </Typography>
-                            <Grid container spacing={3} style={{ textAlign: "center", marginTop: "2%" }}>
-                                <Grid item xs={12} sm={6} >
-                                    <Card className="cardstyle">
-                                        <CardContent>
-                                            <form noValidate autoComplete="off">
-                                                <TextField id="standard-basic" label="Your Name" style={{ width: "100%", marginTop: "5px" }} />
-                                                <br />
-                                                <TextField id="standard-basic" label="Your Email" style={{ width: "100%", marginTop: "5px" }} />
-                                                <br />
-                                                <TextField id="standard-basic" label="Subject" style={{ width: "100%", marginTop: "5px" }} />
-                                                <br />
-                                                <TextField
-                                                    id="standard-textarea"
-                                                    label="Your Message"
-                                                    placeholder="Message"
-                                                    multiline
-                                                    rows={4}
-                                                    style={{ width: "100%", marginTop: "5px" }}
-                                                />
-                                                <br />
-                                                <Button variant="contained" type="submit" style={{ width: "100%", marginTop: "20px", backgroundColor: "#5abb75",color:"white" }}>Send</Button>
-                                            </form>
-                                        </CardContent>
-                                    </Card>
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <img src={pic} alt="" srcSet="" style={{ width: "95%",height:"95%" }} />
-                                </Grid>
-                            </Grid>
-                        </Container>
-                    </div>
-        </StylesProvider>
+        <div className="home-container">
+            <Container>
+                <Typography style={{ textAlign: "center", fontWeight: "700", marginTop: "2%" }} variant="h5" gutterBottom>
+                    Contact Us
+                </Typography>
+                <Grid container spacing={3} style={{ textAlign: "center", marginTop: "2%" }}>
+                    <Grid item xs={12} sm={6} >
+                        <Card className="cardstyle">
+                            <CardContent>
+                                <form noValidate autoComplete="off">
+                                    <TextField id="standard-basic" label="Your Name" style={{ width: "100%", marginTop: "9px" }} />
+                                    <br />
+                                    <TextField id="standard-basic" label="Your Email" style={{ width: "100%", marginTop: "9px" }} />
+                                    <br />
+                                    <TextField id="standard-basic" label="Subject" style={{ width: "100%", marginTop: "9px" }} />
+                                    <br />
+                                    <TextField
+                                        id="standard-textarea"
+                                        label="Your Message"
+                                        placeholder="Message"
+                                        multiline
+                                        rows={4}
+                                        style={{ width: "100%", marginTop: "9px" }}
+                                    />
+                                    <br />
+                                    <Button variant="contained" type="submit" style={{ width: "100%", marginTop: "20px", backgroundColor: "#5abb75", color: "white" }}>Send</Button>
+                                </form>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <img src={pic} alt="" srcSet="" style={{ width: "95%", height: "95%" }} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
     )
 }
 
